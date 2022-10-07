7bafb34294c6ed7ff7ff9de55c4ef6997de423a2
ver7ici <52004913+ver7ici@users.noreply.github.com>
2022-10-07 15:48:26 -0400

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,4 +2,5 @@ import random
 import math
 
 def exponential_random(rate):
+    """Generates an exponential random variable at the given rate"""
     return -(1 / rate) * math.log(1.0 - random.random())
